8f17ac4c451f731d03e8e07aabd5f65c6aca4154
MFAaaaaaa <67424874+MFAaaaaaa@users.noreply.github.com>
2023-07-28 14:46:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -12,11 +12,8 @@ def arg_parser():
     parser.add_argument('--num_class', default=31, type=int)
     parser.add_argument('--source_model_path', default='./model_source/20220425-2142-webcam9_1_resnet50_best.pkl', type=str,
                         help='path to the pre-trained source model')
-    # 20220425-2141-dslr9_1_resnet50_best.pkl
-    # 20220425-2138-amazon9_1_resnet50_best.pkl
-    # 20220425-2142-webcam9_1_resnet50_best.pkl
+
     parser.add_argument('--max_epoch', default=100, type=int)
-    # parser.add_argument('--generator_epoch', default=1000, type=int)
     parser.add_argument('--data_path', default='./dataset/Office-31/amazon/images', type=str,
                         help='path to target data')
     parser.add_argument('--label_path', default='./data_utils/amazon.pkl', type=str)
