5741fbc34e5ddcc7b0842d4753a82ae26fa04b2b
凡安 孟 <17860513085@163.com>
2023-08-01 16:00:45 +0800

    mkdir

--- data_utils/pkl2txt.py
+++ data_utils/pkl2txt.py
@@ -3,12 +3,12 @@ sys.getdefaultencoding()
 import pickle
 import numpy as np
 np.set_printoptions(threshold=1000000000000000)
-path = '../data_utils/dslr_9_1.pkl'
+path = '../data/office31/pkl/dslr_9_1.pkl'
 file = open(path,'rb')
 inf = pickle.load(file,encoding='iso-8859-1')       #读取pkl文件的内容
 # print(inf)
 #fr.close()
 inf=str(inf)
-obj_path = '../data/office31/dslr_val_9_1.txt'
+obj_path = '../data/office31/txt/dslr_val_9_1.txt'
 ft = open(obj_path, 'w')
 ft.write(inf)
